682532c8cec951e489797d7e8efe28cf0dabdfd6
Luke Murray <lukemurray@gmail.com>
2016-10-08 12:10:40 +1100

    make things build for netstandard1.3

--- src/EntityQueryLanguage/DefaultMethodProvider.cs
+++ src/EntityQueryLanguage/DefaultMethodProvider.cs
@@ -12,6 +12,7 @@ namespace EntityQueryLanguage
   ///
   /// Supported Methods:
   ///   List.where(filter)
+  ///   List.filter(filter)
   ///   List.first(filter?)
   ///   List.last(filter?)
   ///   List.take(int)
@@ -20,39 +21,46 @@ namespace EntityQueryLanguage
   ///
   ///   TODO:
   ///   List.orderBy(field desc?, ...)
+  ///   List.sort(field desc?, ...)
   ///   List.select(field, ...)?
   ///   List.isBelow(primary_key)
   ///   List.isAtOrBelow(primary_key)
   ///   List.isAbove(primary_key)
   ///   List.isAtOrAbove(primary_key)
+  ///   string.startsWith(string)
+  ///   string.endsWith(string)
   public class DefaultMethodProvider : IMethodProvider
   {
     // Map of the method names and a function that makes the Expression.Call
   	private Dictionary<string, Func<Expression, Expression, string, Expression[], Expression>> _supportedMethods = new Dictionary<string, Func<Expression, Expression, string, Expression[], Expression>>(StringComparer.OrdinalIgnoreCase)
-	  {
+    {
 	    { "where", MakeWhereMethod },
+      { "filter", MakeWhereMethod },
       { "first", MakeFirstMethod },
       { "last", MakeLastMethod },
       { "take", MakeTakeMethod },
       { "skip", MakeSkipMethod },
       { "count", MakeCountMethod },
-	  };
+    };
 
     public bool EntityTypeHasMethod(Type context, string methodName) {
 	    return _supportedMethods.ContainsKey(methodName);
 	  }
+
     public Expression GetMethodContext(Expression context, string methodName) {
       // some methods have a context of the element type in the list, other is jsut the original context
       // need some way for the method compiler to tells us that
       //  return _supportedMethods[methodName](context);
       return GetContextFromEnumerable(context);
     }
+
   	public Expression MakeCall(Expression context, Expression argContext, string methodName, IEnumerable<Expression> args) {
 	    if (_supportedMethods.ContainsKey(methodName)) {
 		    return _supportedMethods[methodName](context, argContext, methodName, args != null ? args.ToArray() : new Expression[] {});
 	    }
 	    throw new EqlCompilerException($"Unsupported method {methodName}");
 	  }
+
     private static Expression MakeWhereMethod(Expression context, Expression argContext, string methodName, Expression[] args) {
       ExpectArgsCount(1, args, methodName);
       var predicate = args.First();
@@ -60,42 +68,48 @@ namespace EntityQueryLanguage
       var lambda = Expression.Lambda(predicate, argContext as ParameterExpression);
       return MakeExpressionCall(new []{typeof(Queryable), typeof(Enumerable)}, "Where", new Type[] {argContext.Type}, context, lambda);
     }
+
     private static Expression MakeFirstMethod(Expression context, Expression argContext, string methodName, Expression[] args) {
       return MakeOptionalFilterArgumentCall(context, argContext, methodName, args, "First");
     }
+
     private static Expression MakeCountMethod(Expression context, Expression argContext, string methodName, Expression[] args) {
       return MakeOptionalFilterArgumentCall(context, argContext, methodName, args, "Count");
     }
+
     private static Expression MakeOptionalFilterArgumentCall(Expression context, Expression argContext, string methodName, Expression[] args, string actualMethodName) {
       ExpectArgsCountBetween(0, 1, args, methodName);
-      
+
       var allArgs = new List<Expression> { context };
       if (args.Count() == 1) {
         var predicate = args.First();
         ExpectArgTypeToBe(predicate.Type, typeof(bool), methodName);
-        allArgs.Add(Expression.Lambda(predicate, argContext as ParameterExpression)); 
+        allArgs.Add(Expression.Lambda(predicate, argContext as ParameterExpression));
       }
-      
+
       return MakeExpressionCall(new []{typeof(Queryable), typeof(Enumerable)}, actualMethodName, new Type[] {argContext.Type}, allArgs.ToArray());
     }
+
     private static Expression MakeLastMethod(Expression context, Expression argContext, string methodName, Expression[] args) {
       return MakeOptionalFilterArgumentCall(context, argContext, methodName, args, "Last");
     }
+
     private static Expression MakeTakeMethod(Expression context, Expression argContext, string methodName, Expression[] args) {
       ExpectArgsCount(1, args, methodName);
       var amount = args.First();
       ExpectArgTypeToBe(amount.Type, typeof(int), methodName);
-      
+
       return MakeExpressionCall(new []{typeof(Queryable), typeof(Enumerable)}, "Take", new Type[] {argContext.Type}, context, amount);
     }
+
     private static Expression MakeSkipMethod(Expression context, Expression argContext, string methodName, Expression[] args) {
       ExpectArgsCount(1, args, methodName);
       var amount = args.First();
       ExpectArgTypeToBe(amount.Type, typeof(int), methodName);
-      
+
       return MakeExpressionCall(new []{typeof(Queryable), typeof(Enumerable)}, "Skip", new Type[] {argContext.Type}, context, amount);
     }
-    
+
     private static Expression MakeExpressionCall(Type[] types, string methodName, Type[] genericTypes, params Expression[] parameters) {
       foreach (var t in types) {
         // Please tell me a better way to do this!
@@ -110,7 +124,7 @@ namespace EntityQueryLanguage
       var typesStr = string.Join<Type>(", ", types);
       throw new EqlCompilerException($"Could not find extension method {methodName} on types {typesStr}");
     }
-    
+
     private static Expression GetContextFromEnumerable(Expression context) {
       if (context.Type.IsEnumerable()) {
         return Expression.Parameter(context.Type.GetGenericArguments()[0]);
@@ -120,14 +134,17 @@ namespace EntityQueryLanguage
         return Expression.Parameter(t);
       return context;
     }
+
     private static void ExpectArgsCount(int count, Expression[] args, string method) {
       if (args.Count() != count)
         throw new EqlCompilerException($"Method '{method}' expects {count} argument(s) but {args.Count()} were supplied");
     }
+
     private static void ExpectArgsCountBetween(int low, int high, Expression[] args, string method) {
       if (args.Count() < low || args.Count() > high)
         throw new EqlCompilerException($"Method '{method}' expects {low}-{high} argument(s) but {args.Count()} were supplied");
     }
+
     private static void ExpectArgTypeToBe(Type argType, Type expected, string methodName) {
       if (argType != expected)
         throw new EqlCompilerException($"Method '{methodName}' expects parameter that evaluates to a '{expected}' result but found result type '{argType}'");
---
+++ src/EntityQueryLanguage/EqlGrammerVisitor.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+using EntityQueryLanguage.Grammer;
+using EntityQueryLanguage.Extensions;
+
+namespace EntityQueryLanguage
+{
+  internal class EqlGrammerVisitor : EqlGrammerBaseVisitor<Expression> {
+    private Expression _currentContext;
+    private ISchemaProvider _schemaProvider;
+    private IMethodProvider _methodProvider;
+
+    public EqlGrammerVisitor(Expression expression, ISchemaProvider schemaProvider, IMethodProvider methodProvider) {
+      _currentContext = expression;
+      _schemaProvider = schemaProvider;
+      _methodProvider = methodProvider;
+    }
+
+    public override Expression VisitBinary(EqlGrammerParser.BinaryContext context) {
+      var left = Visit(context.left);
+      var right = Visit(context.right);
+      var op = MakeOperator(context.op.GetText());
+      // we may need to do some converting here
+      if (left.Type != right.Type) {
+        return ConvertLeftOrRight(op, left, right);
+      }
+
+      if (op == ExpressionType.Add && left.Type == typeof(string) && right.Type == typeof(string)) {
+        return Expression.Call(null, typeof(string).GetMethod("Concat", new[] { typeof(string), typeof(string) }), left, right);
+      }
+
+      return Expression.MakeBinary(op, left, right);
+    }
+
+    public override Expression VisitExpr(EqlGrammerParser.ExprContext context) {
+      var r = Visit(context.body);
+      return r;
+    }
+
+    public override Expression VisitCallPath(EqlGrammerParser.CallPathContext context) {
+      var startingContext = _currentContext;
+      Expression exp = null;
+      foreach (var child in context.children) {
+        var r = Visit(child);
+        if (r == null)
+          continue;
+
+        exp = r;
+        _currentContext = exp;
+      }
+      _currentContext = startingContext;
+      return exp;
+    }
+
+    public override Expression VisitIdentity(EqlGrammerParser.IdentityContext context) {
+      // check that the schema has the property for the context
+      var field = context.GetText();
+      //TODO - need to get the mapped name for the type to check for fields to support mapped schema too
+      if (!_schemaProvider.TypeHasField(_schemaProvider.GetSchemaTypeNameForRealType(_currentContext.Type), field)) {
+        throw new EqlCompilerException($"Field or property '{field}' not found on current context '{_currentContext.Type.Name}'");
+      }
+      var exp = _schemaProvider.GetExpressionForField(_currentContext, _currentContext.Type.Name, field);
+      return exp;
+    }
+
+    public override Expression VisitInt(EqlGrammerParser.IntContext context) {
+      return Expression.Constant(Int32.Parse(context.GetText()));
+    }
+
+    public override Expression VisitDecimal(EqlGrammerParser.DecimalContext context) {
+      return Expression.Constant(Decimal.Parse(context.GetText()));
+    }
+
+    public override Expression VisitString(EqlGrammerParser.StringContext context) {
+      return Expression.Constant(context.GetText().Trim('\''));
+    }
+
+    public override Expression VisitIfThenElse(EqlGrammerParser.IfThenElseContext context) {
+      return Expression.Condition(CheckConditionalTest(Visit(context.test)), Visit(context.ifTrue), Visit(context.ifFalse));
+    }
+
+    public override Expression VisitIfThenElseInline(EqlGrammerParser.IfThenElseInlineContext context) {
+      return Expression.Condition(CheckConditionalTest(Visit(context.test)), Visit(context.ifTrue), Visit(context.ifFalse));
+    }
+
+    public override Expression VisitCall(EqlGrammerParser.CallContext context) {
+      var method = context.method.GetText();
+      if (!_methodProvider.EntityTypeHasMethod(_currentContext.Type, method))
+        throw new EqlCompilerException($"Method '{method}' not found on current context '{_currentContext.Type.Name}'");
+      // Keep the current context
+      var outerContext = _currentContext;
+      // some methods might have a different inner context (IEnumerable etc)
+      var methodArgContext = _methodProvider.GetMethodContext(_currentContext, method);
+      _currentContext = methodArgContext;
+      // Compile the arguments with the new context
+      var args = context.arguments?.children.Select(c => Visit(c)).ToList();
+      // build our method call
+      var call = _methodProvider.MakeCall(outerContext, methodArgContext, method, args);
+      _currentContext = call;
+      return call;
+    }
+
+    public override Expression VisitArgs(EqlGrammerParser.ArgsContext context) {
+      return VisitChildren(context);
+    }
+    //  public override Expression VisitCallOrId(EqlGrammerParser.CallOrIdContext context) {
+    //    return VisitChildren(context);
+    //  }
+    //  public override Expression VisitConstant(EqlGrammerParser.ConstantContext context) {
+    //    return VisitChildren(context);
+    //  }
+    //  public override Expression VisitOperator(EqlGrammerParser.OperatorContext context) {
+    //    return VisitChildren(context);
+    //  }
+    //  public override Expression VisitExpression(EqlGrammerParser.ExpressionContext context) {
+    //    return VisitChildren(context);
+    //  }
+    //  public override Expression VisitStartRule(EqlGrammerParser.StartRuleContext context) {
+    //    Console.WriteLine($"----VisitStartRule {context.GetText()}");
+    //    return VisitChildren(context);
+    //  }
+
+    /// Implements rules about comparing non-matching types.
+    /// Nullable vs. non-nullable - the non-nullable gets converted to nullable
+    /// int vs. uint - the uint gets down cast to int
+    /// more to come...
+    private Expression ConvertLeftOrRight(ExpressionType op, Expression left, Expression right) {
+      if (left.Type.IsNullableType() && !right.Type.IsNullableType())
+        right = Expression.Convert(right, left.Type);
+      else if (right.Type.IsNullableType() && !left.Type.IsNullableType())
+        left = Expression.Convert(left, right.Type);
+
+      else if (left.Type == typeof(int) && right.Type == typeof(uint))
+        right = Expression.Convert(right, left.Type);
+      else if (left.Type == typeof(uint) && right.Type == typeof(int))
+        left = Expression.Convert(left, right.Type);
+
+      return Expression.MakeBinary(op, left, right);
+    }
+
+    private Expression CheckConditionalTest(Expression test) {
+      if (test.Type != typeof(bool))
+        throw new EqlCompilerException($"Expected boolean value in conditional test but found '{test}'");
+      return test;
+    }
+
+    private ExpressionType MakeOperator(string op) {
+      switch (op) {
+        case "=": return ExpressionType.Equal;
+        case "+": return ExpressionType.Add;
+        case "-": return ExpressionType.Subtract;
+        case "%": return ExpressionType.Modulo;
+        case "^": return ExpressionType.Power;
+        case "and": return ExpressionType.AndAlso;
+        case "*": return ExpressionType.Multiply;
+        case "or": return ExpressionType.OrElse;
+        case "<=": return ExpressionType.LessThanOrEqual;
+        case ">=": return ExpressionType.GreaterThanOrEqual;
+        case "<": return ExpressionType.LessThan;
+        case ">": return ExpressionType.GreaterThan;
+        default: throw new EqlCompilerException($"Unsupported binary operator '{op}'");
+      }
+    }
+  }
+}
