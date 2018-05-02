b3fde5dd67488572a41275df771087afce23a6fd
Luke Murray <lukemurray@gmail.com>
2018-05-02 19:47:26 +1000

    relax verion

--- src/demo/Startup.cs
+++ src/demo/Startup.cs
@@ -30,7 +30,7 @@ namespace demo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DemoContext>(opt => opt.UseInMemoryDatabase());
+            services.AddDbContext<DemoContext>(opt => opt.UseInMemoryDatabase("demo-db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
