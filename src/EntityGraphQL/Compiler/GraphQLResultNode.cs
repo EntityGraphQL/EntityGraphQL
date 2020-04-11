4ce088336d09caad6760cceb61f3ff8c829855d8
Jeremy R. Travis <jtravis28@gmail.com>
2020-04-11 12:17:30 -0400

    Model Vaildation - Mutation

--- src/EntityGraphQL/Compiler/GraphQLResultNode.cs
+++ src/EntityGraphQL/Compiler/GraphQLResultNode.cs
@@ -63,6 +63,9 @@ namespace EntityGraphQL.Compiler
                 result.Data[node.Name] = data;
             }
 
+            if (GraphQLVaildation.Errors.Count > 0)
+                result.Errors.AddRange(GraphQLVaildation.Errors);
+
             return result;
         }
     }
