7e1bdaee8274f995e0c7c92c0ed73cfa33ee9c94
Luke Murray <lukemurray@gmail.com>
2022-07-04 20:53:55 +1000

    clean up docs deps

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -37,17 +37,6 @@ const plugins = [
       extensions: [".mdx", ".md"]
     }
   },
-  {
-    resolve: `gatsby-plugin-gtag`,
-    options: {
-      // your google analytics tracking id
-      trackingId: config.gatsby.gaTrackingId,
-      // Puts tracking script in the head instead of the body
-      head: true,
-      // enable ip anonymization
-      anonymize: false,
-    },
-  },
 ];
 // check and add algolia
 if (config.header.search && config.header.search.enabled && config.header.search.algoliaAppId && config.header.search.algoliaAdminKey) {
@@ -62,21 +51,8 @@ if (config.header.search && config.header.search.enabled && config.header.search
   }
   )
 }
-// check and add pwa functionality
-if (config.pwa && config.pwa.enabled && config.pwa.manifest) {
-  plugins.push({
-    resolve: `gatsby-plugin-manifest`,
-    options: { ...config.pwa.manifest },
-  });
-  plugins.push({
-    resolve: 'gatsby-plugin-offline',
-    options: {
-      appendScript: require.resolve(`./src/custom-sw-code.js`),
-    },
-  });
-} else {
-  plugins.push('gatsby-plugin-remove-serviceworker');
-}
+
+plugins.push('gatsby-plugin-remove-serviceworker');
 
 // check and remove trailing slash
 if (config.gatsby && !config.gatsby.trailingSlash) {
