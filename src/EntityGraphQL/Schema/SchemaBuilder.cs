55377b73a5ab502c7e924ec14652759a9e2df130
Luke Murray <lukemurray@gmail.com>
2022-07-07 21:29:48 +1000

    update comment docs

--- src/EntityGraphQL/Schema/SchemaBuilder.cs
+++ src/EntityGraphQL/Schema/SchemaBuilder.cs
@@ -13,21 +13,37 @@ using EntityGraphQL.Schema.FieldExtensions;
 
 namespace EntityGraphQL.Schema
 {
+    /// <summary>
+    /// Options used by the SchemaBuilder factory with creating the SchemaProvider
+    /// </summary>
     public class SchemaBuilderSchemaOptions
     {
         public static readonly Func<string, string> DefaultFieldNamer = name =>
         {
             return name[..1].ToLowerInvariant() + name[1..];
         };
-
+        /// <summary>
+        /// Function to name fields when reading the properties from reflection. Default is camelCase
+        /// </summary>
         public Func<string, string> FieldNamer { get; set; } = DefaultFieldNamer;
+        /// <summary>
+        /// If true (default) schema introspection will be enabled for the schema
+        /// </summary>
         public bool IntrospectionEnabled { get; set; } = true;
-
+        /// <summary>
+        /// 
+        /// </summary>
         public IGqlAuthorizationService AuthorizationService { get; set; } = new RoleBasedAuthorization();
     }
 
+    /// <summary>
+    /// Options used by SchemaBuilder when reflection the object graph to auto create schema types & fields
+    /// </summary>
     public class SchemaBuilderOptions
     {
+        /// <summary>
+        /// List properties or field names to ignore. Default includes a list of EF properties
+        /// </summary>
         public HashSet<string> IgnoreProps { get; set; } = new()
         {
             "Database",
@@ -35,14 +51,31 @@ namespace EntityGraphQL.Schema
             "ChangeTracker",
             "ContextId"
         };
-
+        /// <summary>
+        /// List of dotnet type names to ignore when adding types to the schema
+        /// </summary>
         public HashSet<string> IgnoreTypes { get; set; } = new()
         {
             "String",
             "Byte[]"
         };
-        public bool AutoCreateIdArguments { get; set; } = true;
+        /// <summary>
+        /// If true when SchemaBuilder encounters a field that returns a list of entities and the entity has a property 
+        /// or field name Id t will also create a schema field with a singular name and an argument of id for that entity.
+        /// e.g. if it sees IEnumerable<Person> People; It will create the schema fields
+        /// {
+        ///   people: [Person]
+        ///   person(id: ID!): Person
+        /// }
+        /// </summary>
+        public bool AutoCreateFieldWithIdArguments { get; set; } = true;
+        /// <summary>
+        /// If true (default) and an enum type is encountered during reflection of the object graph it will be added to the schema as an Enum
+        /// </summary>
         public bool AutoCreateEnumTypes { get; set; } = true;
+        /// <summary>
+        /// If true (default) and an object type is encountered during reflection of the object graph it will be added to the schema as a Type including it's fields
+        /// </summary>
         public bool AutoCreateNewComplexTypes { get; set; } = true;
     }
 
@@ -216,7 +249,7 @@ namespace EntityGraphQL.Schema
             var returnTypeInfo = schema.GetCustomTypeMapping(le.ReturnType) ?? new GqlTypeInfo(() => schema.GetSchemaType(baseReturnType, null), le.Body.Type, prop);
             var field = new Field(schema, schema.SchemaFieldNamer(prop.Name), le, description, returnTypeInfo, requiredClaims);
 
-            if (options.AutoCreateIdArguments)
+            if (options.AutoCreateFieldWithIdArguments)
             {
                 // add non-pural field with argument of ID
                 var idArgField = AddFieldWithIdArgumentIfExists(schema, prop.ReflectedType, field);
