23068b0a3365a207e77eeba5cd011203a58d2e89
Betty <sam.mckoy@enlighten.co.nz>
2022-10-22 11:55:33 +1300

    complicate the examples more

--- src/Benchmarks/ParameterReplacerBenchmarks.cs
+++ src/Benchmarks/ParameterReplacerBenchmarks.cs
@@ -22,9 +22,11 @@ namespace Benchmarks
             public float? RatingMax { get; set; }
             public DateTime? ReleasedBefore { get; set; }
             public DateTime? ReleasedAfter { get; set; }
+            public Guid? DirectorId { get; set; }
             public string? DirectorName { get; set; }
+            public Guid? ActorId { get; set; }
             public string? ActorName { get; set; }
-            public string Genre { get; set; }
+            public string[] Genres { get; set; }
         }
 
         Expression<Func<BenchmarkContext, IEnumerable<Movie>>> _node;
@@ -57,21 +59,23 @@ namespace Benchmarks
                     .WhereWhen(i => i.Released > args.ReleasedAfter, args.ReleasedAfter.HasValue)
                     .WhereWhen(i => i.Released < args.ReleasedBefore, args.ReleasedBefore.HasValue)
                     .WhereWhen(i => i.Name == args.Name, !string.IsNullOrWhiteSpace(args.Name))
-                    .WhereWhen(i => i.Genre.Name == args.Genre, args.Genre != null)
+                    .WhereWhen(i => args.Genres.Contains(i.Genre.Name), args.Genres != null)
                     ;
 
             _node4 = (ctx, args) => ctx.Set<Movie>()
                     .AsSplitQuery()
                     .AsNoTracking()
+                    .IgnoreQueryFilters()
                     .WhereWhen(i => i.Name == args.Name, !string.IsNullOrWhiteSpace(args.Name))
                     .WhereWhen(i => i.Director.FirstName == args.DirectorName, !string.IsNullOrWhiteSpace(args.DirectorName))
-                    .WhereWhen(i => i.Actors.Any(x => x.FirstName == args.ActorName), !string.IsNullOrWhiteSpace(args.ActorName))
+                    .WhereWhen(i => i.Director.Id == args.DirectorId, args.DirectorId.HasValue)
+                    .WhereWhen(i => i.Actors.Any(x => x.Id == args.ActorId), !string.IsNullOrWhiteSpace(args.ActorName))
+                    .WhereWhen(i => i.Actors.Any(x => x.FirstName == args.ActorName), args.ActorId.HasValue)
                     .WhereWhen(i => i.Rating > args.RatingMin, args.RatingMin.HasValue)
                     .WhereWhen(i => i.Rating < args.RatingMax, args.RatingMax.HasValue)
                     .WhereWhen(i => i.Released > args.ReleasedAfter, args.ReleasedAfter.HasValue)
                     .WhereWhen(i => i.Released < args.ReleasedBefore, args.ReleasedBefore.HasValue)
-                    .WhereWhen(i => i.Name == args.Name, !string.IsNullOrWhiteSpace(args.Name))
-                    .WhereWhen(i => i.Genre.Name == args.Genre, args.Genre != null)                    
+                    .WhereWhen(i => args.Genres.Contains(i.Genre.Name), args.Genres.Length > 0)
                     ;
         }
 
