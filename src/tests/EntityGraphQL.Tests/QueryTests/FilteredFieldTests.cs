8893d7d3d5b1b778ad57b6c6b519b5fa43a4fa01
Luke Murray <lukemurray@gmail.com>
2024-04-27 12:22:35 +1000

    add test case for #365

--- src/tests/EntityGraphQL.Tests/QueryTests/FilteredFieldTests.cs
+++ src/tests/EntityGraphQL.Tests/QueryTests/FilteredFieldTests.cs
@@ -1,138 +1,140 @@
-using Xunit;
-using System.Collections.Generic;
-using EntityGraphQL.Schema;
-using System.Linq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using EntityGraphQL.Extensions;
+using EntityGraphQL.Schema;
 using EntityGraphQL.Schema.FieldExtensions;
 using Microsoft.Extensions.DependencyInjection;
+using Xunit;
 
-namespace EntityGraphQL.Tests
+namespace EntityGraphQL.Tests;
+
+public class FilteredFieldTests
 {
-    public class FilteredFieldTests
+    [Fact]
+    public void TestUseConstFilter()
     {
-        [Fact]
-        public void TestUseConstFilter()
+        var schema = SchemaBuilder.FromObject<TestDataContext>();
+        schema.Query().ReplaceField("projects", new { search = (string)null }, (ctx, args) => ctx.Projects.OrderBy(p => p.Id), "List of projects");
+
+        Func<Task, bool> TaskFilter = t => t.IsActive == true;
+        schema.Type<Project>().ReplaceField("tasks", p => p.Tasks.Where(TaskFilter), "Active tasks");
+
+        var gql = new QueryRequest
         {
-            var schema = SchemaBuilder.FromObject<TestDataContext>();
-            schema.Query().ReplaceField("projects",
-                new
-                {
-                    search = (string)null
-                },
-                (ctx, args) => ctx.Projects.OrderBy(p => p.Id),
-                "List of projects");
-
-            Func<Task, bool> TaskFilter = t => t.IsActive == true;
-            schema.Type<Project>().ReplaceField("tasks", p => p.Tasks.Where(TaskFilter), "Active tasks");
-
-            var gql = new QueryRequest
-            {
-                Query = @"query {
+            Query =
+                @"query {
                     projects {
                         tasks { id }
                     }
                 }"
-            };
+        };
 
-            var context = new TestDataContext
-            {
-                Projects = new List<Project>
-                {
-                    new Project
-                    {
-                        Tasks = new List<Task> { new Task() },
-                    }
-                },
-            };
+        var context = new TestDataContext { Projects = new List<Project> { new Project { Tasks = new List<Task> { new Task() }, } }, };
 
-            var res = schema.ExecuteRequestWithContext(gql, context, null, null);
-            Assert.Null(res.Errors);
-            dynamic project = Enumerable.ElementAt((dynamic)res.Data["projects"], 0);
-            Type projectType = project.GetType();
-            Assert.Single(projectType.GetFields());
-            Assert.Equal("tasks", projectType.GetFields()[0].Name);
-        }
+        var res = schema.ExecuteRequestWithContext(gql, context, null, null);
+        Assert.Null(res.Errors);
+        dynamic project = Enumerable.ElementAt((dynamic)res.Data["projects"], 0);
+        Type projectType = project.GetType();
+        Assert.Single(projectType.GetFields());
+        Assert.Equal("tasks", projectType.GetFields()[0].Name);
+    }
 
-        [Fact]
-        public void TestWhereWhenOnNonRootField()
-        {
-            var schema = SchemaBuilder.FromObject<TestDataContext>();
+    [Fact]
+    public void TestWhereWhenOnNonRootField()
+    {
+        var schema = SchemaBuilder.FromObject<TestDataContext>();
 
-            schema.Type<Project>().ReplaceField("tasks",
-                new
-                {
-                    like = (string)null
-                },
+        schema
+            .Type<Project>()
+            .ReplaceField(
+                "tasks",
+                new { like = (string)null },
                 (project, args) => project.Tasks.WhereWhen(t => t.Name.Contains(args.like), !string.IsNullOrEmpty(args.like)),
-                "List of project tasks");
+                "List of project tasks"
+            );
 
-            var gql = new QueryRequest
-            {
-                Query = @"{
+        var gql = new QueryRequest
+        {
+            Query =
+                @"{
                     projects {
                         tasks(like: ""h"") { name }
                     }
                 }"
-            };
+        };
 
-            var context = new TestDataContext
-            {
-                Projects = new List<Project>
-                {
-                    new Project
-                    {
-                        Tasks = new List<Task>
-                        {
-                            new Task { Name = "hello" },
-                            new Task { Name = "world" },
-                        },
-                        Description = "Hello"
-                    }
-                },
-            };
-
-            var res = schema.ExecuteRequestWithContext(gql, context, null, null);
-            Assert.Null(res.Errors);
-            dynamic project = Enumerable.First((dynamic)res.Data["projects"]);
-            Type projectType = project.GetType();
-            Assert.Single(projectType.GetFields());
-            Assert.Equal("tasks", projectType.GetFields()[0].Name);
-        }
-
-        [Fact(Skip = "Not implemented yet. Need to know that the filter uses the service field age")]
-        public void TestOffsetPagingWithOthersAndServices()
+        var context = new TestDataContext
         {
-            var schema = SchemaBuilder.FromObject<TestDataContext>();
-            var data = new TestDataContext();
-            data.People.Add(new Person { Id = 1, Name = "Jill", LastName = "Frank", Birthday = DateTime.Now.AddYears(22) });
-            data.People.Add(new Person { Id = 2, Name = "Cheryl", LastName = "Frank", Birthday = DateTime.Now.AddYears(10) });
-
-            schema.Query().ReplaceField("people", ctx => ctx.People, "Return list of people")
-                .UseFilter();
-            schema.Type<Person>().AddField("age", "Persons age")
-                .Resolve<AgeService>((person, ager) => ager.GetAge(person.Birthday));
-            var gql = new QueryRequest
+            Projects = new List<Project>
             {
-                Query = @"{
+                new Project
+                {
+                    Tasks = new List<Task>
+                    {
+                        new Task { Name = "hello" },
+                        new Task { Name = "world" },
+                    },
+                    Description = "Hello"
+                }
+            },
+        };
+
+        var res = schema.ExecuteRequestWithContext(gql, context, null, null);
+        Assert.Null(res.Errors);
+        dynamic project = Enumerable.First((dynamic)res.Data["projects"]);
+        Type projectType = project.GetType();
+        Assert.Single(projectType.GetFields());
+        Assert.Equal("tasks", projectType.GetFields()[0].Name);
+    }
+
+    [Fact(Skip = "Not implemented yet. Need to know that the filter uses the service field age")]
+    public void TestOffsetPagingWithOthersAndServices()
+    {
+        var schema = SchemaBuilder.FromObject<TestDataContext>();
+        var data = new TestDataContext();
+        data.People.Add(
+            new Person
+            {
+                Id = 1,
+                Name = "Jill",
+                LastName = "Frank",
+                Birthday = DateTime.Now.AddYears(22)
+            }
+        );
+        data.People.Add(
+            new Person
+            {
+                Id = 2,
+                Name = "Cheryl",
+                LastName = "Frank",
+                Birthday = DateTime.Now.AddYears(10)
+            }
+        );
+
+        schema.Query().ReplaceField("people", ctx => ctx.People, "Return list of people").UseFilter();
+        schema.Type<Person>().AddField("age", "Persons age").Resolve<AgeService>((person, ager) => ager.GetAge(person.Birthday));
+        var gql = new QueryRequest
+        {
+            Query =
+                @"{
                     people(filter: ""age > 21"") {
                         name id age lastName
                     }
                 }",
-            };
+        };
 
-            var serviceCollection = new ServiceCollection();
-            var ager = new AgeService();
-            serviceCollection.AddSingleton(ager);
+        var serviceCollection = new ServiceCollection();
+        var ager = new AgeService();
+        serviceCollection.AddSingleton(ager);
 
-            var result = schema.ExecuteRequestWithContext(gql, data, serviceCollection.BuildServiceProvider(), null);
-            Assert.Null(result.Errors);
+        var result = schema.ExecuteRequestWithContext(gql, data, serviceCollection.BuildServiceProvider(), null);
+        Assert.Null(result.Errors);
 
-            dynamic people = result.Data["people"];
-            Assert.Equal(1, Enumerable.Count(people));
-            var person1 = Enumerable.ElementAt(people, 0);
-            Assert.Equal("Frank", person1.lastName);
-            Assert.Equal("Jill", person1.name);
-        }
+        dynamic people = result.Data["people"];
+        Assert.Equal(1, Enumerable.Count(people));
+        var person1 = Enumerable.ElementAt(people, 0);
+        Assert.Equal("Frank", person1.lastName);
+        Assert.Equal("Jill", person1.name);
     }
 }
