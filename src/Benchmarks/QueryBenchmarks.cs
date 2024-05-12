b2d06362d7d42ccd3fb14aff67ce73e2b91f6e65
Luke Murray <lukemurray@gmail.com>
2024-05-12 10:50:03 +1000

    add benchmark and update changelog

--- src/Benchmarks/QueryBenchmarks.cs
+++ src/Benchmarks/QueryBenchmarks.cs
@@ -4,38 +4,46 @@ using EntityGraphQL;
 using EntityGraphQL.Extensions;
 using EntityGraphQL.Schema;
 
-namespace Benchmarks
+namespace Benchmarks;
+
+/// <summary>
+/// Was used to test if the replacement of the Antlr4 parser of the GraphQL query document was faster. Yes it was!
+/// </summary>
+[ShortRunJob]
+public class QueryBenchmarks : BaseBenchmark
 {
-    /// <summary>
-    /// Was used to test if the replacement of the Antlr4 parser was faster. Yes it was!
-    /// </summary>
-    [ShortRunJob]
-    public class QueryBenchmarks : BaseBenchmark
+    [Benchmark]
+    public void Query_SingleObjectWithArg()
     {
-        [Benchmark]
-        public void Query_SingleObjectWithArg()
-        {
-            RunQuery(GetContext(), new QueryRequest
+        RunQuery(
+            GetContext(),
+            new QueryRequest
             {
-                Query = @"{
+                Query =
+                    @"{
                     movie(id: ""433f8132-a7a5-40c9-96c2-e2122fb72e68"") {
                         id name released
                     }
                 }"
-            }, new ExecutionOptions
+            },
+            new ExecutionOptions
             {
 #if DEBUG
                 NoExecution = true
 #endif
-            });
-        }
+            }
+        );
+    }
 
-        [Benchmark]
-        public void Query_SingleObjectWithArg_IncludeSubObject()
-        {
-            RunQuery(GetContext(), new QueryRequest
+    [Benchmark]
+    public void Query_SingleObjectWithArg_IncludeSubObject()
+    {
+        RunQuery(
+            GetContext(),
+            new QueryRequest
             {
-                Query = @"{
+                Query =
+                    @"{
                     movie(id: ""1deb79a1-59b1-4360-8d95-04bd7107ad8c"") {
                         id name released
                         director {
@@ -43,19 +51,25 @@ namespace Benchmarks
                         }
                     }
                 }"
-            }, new ExecutionOptions
+            },
+            new ExecutionOptions
             {
 #if DEBUG
                 NoExecution = true
 #endif
-            });
-        }
-        [Benchmark]
-        public void Query_SingleObjectWithArg_IncludeSubObjectAndList()
-        {
-            RunQuery(GetContext(), new QueryRequest
+            }
+        );
+    }
+
+    [Benchmark]
+    public void Query_SingleObjectWithArg_IncludeSubObjectAndList()
+    {
+        RunQuery(
+            GetContext(),
+            new QueryRequest
             {
-                Query = @"{
+                Query =
+                    @"{
                     movie(id: ""077b3041-307a-42ba-9ffe-1121fcfc918b"") {
                         id name released
                         director {
@@ -66,58 +80,65 @@ namespace Benchmarks
                         }
                     }
                 }"
-            }, new ExecutionOptions
+            },
+            new ExecutionOptions
             {
 #if DEBUG
                 NoExecution = true
 #endif
-            });
-        }
-        [Benchmark]
-        public void Query_List()
-        {
-            RunQuery(GetContext(), new QueryRequest
+            }
+        );
+    }
+
+    [Benchmark]
+    public void Query_List()
+    {
+        RunQuery(
+            GetContext(),
+            new QueryRequest
             {
-                Query = @"{
+                Query =
+                    @"{
                     movies {
                         id name released
                     }
                 }"
-            }, new ExecutionOptions
+            },
+            new ExecutionOptions
             {
 #if DEBUG
                 NoExecution = true
 #endif
-            });
-        }
-        [GlobalSetup(Target = nameof(Query_ListWithTakeArg))]
-        public void ModifyField()
-        {
-            Schema.Query().ReplaceField(
-                "movies",
-                new
-                {
-                    take = (int?)null
-                },
-                (ctx, args) => ctx.Movies.Take(args.take),
-                "List of movies");
-        }
-        [Benchmark]
-        public void Query_ListWithTakeArg()
-        {
-            RunQuery(GetContext(), new QueryRequest
+            }
+        );
+    }
+
+    [GlobalSetup(Target = nameof(Query_ListWithTakeArg))]
+    public void ModifyField()
+    {
+        Schema.Query().ReplaceField("movies", new { take = (int?)null }, (ctx, args) => ctx.Movies.Take(args.take), "List of movies");
+    }
+
+    [Benchmark]
+    public void Query_ListWithTakeArg()
+    {
+        RunQuery(
+            GetContext(),
+            new QueryRequest
             {
-                Query = @"{
+                Query =
+                    @"{
                     movies(take: 10) {
                         id name released
                     }
                 }"
-            }, new ExecutionOptions
+            },
+            new ExecutionOptions
             {
 #if DEBUG
                 NoExecution = true
 #endif
-            });
-        }
+            }
+        );
     }
 }
