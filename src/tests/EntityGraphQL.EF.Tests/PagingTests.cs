08221e5c4dba3d043c8ffd915800adcc8b962664
Luke Murray <lukemurray@gmail.com>
2025-04-27 13:35:07 +1000

    test self referencing offset paging

--- src/tests/EntityGraphQL.EF.Tests/PagingTests.cs
+++ src/tests/EntityGraphQL.EF.Tests/PagingTests.cs
@@ -8,7 +8,7 @@ namespace EntityGraphQL.EF.Tests;
 public class PagingTests
 {
     [Fact]
-    public void Test1ToManySelfReference()
+    public void Test1ToManySelfReferenceConnection()
     {
         var schema = SchemaBuilder.FromObject<TestDbContext>();
         using var factory = new TestDbContextFactory();
@@ -57,5 +57,55 @@ public class PagingTests
         Assert.Equal(1, actors.edges[1].node.children.Count);
         Assert.Equal("Child", actors.edges[1].node.name);
         Assert.Equal("Parent", actors.edges[1].node.children[0].name);
+    }
+
+    [Fact]
+    public void Test1ToManySelfReferenceOffset()
+    {
+        var schema = SchemaBuilder.FromObject<TestDbContext>();
+        using var factory = new TestDbContextFactory();
+        var data = factory.CreateContext();
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton(data);
+        data.Actors.Add(new Actor("Parent") { Id = 1 });
+        data.SaveChanges();
+        data.Actors.Add(
+            new Actor("Child")
+            {
+                Id = 2,
+                Children = new List<Actor> { data.Actors.First() },
+            }
+        );
+        data.SaveChanges();
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        schema.Query().ReplaceField("actors", ctx => ctx.Actors.OrderBy(p => p.Id), "Return list of people with paging metadata").UseOffsetPaging();
+        var gql = new QueryRequest
+        {
+            Query =
+                @"{
+                    actors {
+                        totalItems
+                        items {
+                            name
+                            children {
+                                name
+                            }
+                        }
+                    }
+                }",
+        };
+
+        var result = schema.ExecuteRequestWithContext(gql, data, null, null);
+        Assert.Null(result.Errors);
+
+        dynamic actors = result.Data!["actors"]!;
+        Assert.Equal(data.Actors.Count(), Enumerable.Count(actors.items));
+        Assert.Equal(data.Actors.Count(), actors.totalItems);
+        Assert.Empty(actors.items[0].children);
+        Assert.Equal("Parent", actors.items[0].name);
+        Assert.Equal(1, actors.items[1].children.Count);
+        Assert.Equal("Child", actors.items[1].name);
+        Assert.Equal("Parent", actors.items[1].children[0].name);
     }
 }
