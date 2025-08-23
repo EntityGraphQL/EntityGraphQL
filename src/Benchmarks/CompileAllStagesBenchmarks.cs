97ca3bdc9b730901124ac317624cff5752638761
Luke Murray <lukemurray@gmail.com>
2025-08-23 13:15:37 +1000

    updates to bencmarks

--- src/Benchmarks/CompileAllStagesBenchmarks.cs
+++ src/Benchmarks/CompileAllStagesBenchmarks.cs
@@ -9,18 +9,6 @@ namespace Benchmarks;
 /// On a Apple M1 Max 64GB ram
 /// Command: `dotnet run -c Debug --framework net8.0` to skip execution
 ///
-/// 5.2.1 (5.0 & 5.1 were basically the same)
-/// |         Method |      Mean |    Error |   StdDev |   Gen 0 | Allocated |
-/// |--------------- |----------:|---------:|---------:|--------:|----------:|
-/// | CompileNoCache | 116.21 us | 0.399 us | 0.354 us | 42.4805 |     87 KB |
-/// |   CompileCache |  95.66 us | 0.388 us | 0.363 us | 33.5693 |     69 KB |
-///
-/// 5.3.0
-/// |         Method |      Mean |    Error |   StdDev |   Gen 0 | Allocated |
-/// |--------------- |----------:|---------:|---------:|--------:|----------:|
-/// | CompileNoCache | 125.85 us | 0.650 us | 0.543 us | 44.4336 |     91 KB |
-/// |   CompileCache |  97.15 us | 0.396 us | 0.371 us | 33.9355 |     69 KB |
-///
 /// 5.6.0 with net9.0
 /// On a Apple M1 Max 64GB ram
 /// Command: `dotnet run -c Debug --framework net8.0` to skip execution
@@ -29,6 +17,15 @@ namespace Benchmarks;
 /// |--------------- |---------:|---------:|---------:|--------:|-------:|----------:|
 /// | CompileNoCache | 97.04 us | 0.795 us | 0.744 us | 15.6250 | 0.9766 |  95.79 KB |
 /// | CompileCache   | 80.42 us | 0.190 us | 0.177 us | 12.5732 | 0.4883 |  77.08 KB |
+///
+/// BenchmarkDotNet v0.15.2, macOS 26.0 (25A5338b) [Darwin 25.0.0]
+/// Apple M1 Max, 1 CPU, 10 logical and 10 physical cores
+/// .NET SDK 9.0.301
+/// | Method         | Mean     | Error    | StdDev   | Gen0    | Gen1   | Allocated |
+/// |--------------- |---------:|---------:|---------:|--------:|-------:|----------:|
+/// | CompileNoCache | 93.56 us | 1.776 us | 1.662 us | 15.3809 | 0.7324 |  94.25 KB |
+/// | CompileCache   | 77.24 us | 1.524 us | 1.425 us | 12.2070 | 0.2441 |  75.39 KB |
+///
 /// </summary>
 [MemoryDiagnoser]
 public class CompileAllStagesBenchmarks : BaseBenchmark
